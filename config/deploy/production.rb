49f1df700931768be1d7c9803c4a7f4bab19d18c
Marcol L <fwneurona@gmail.com>
2021-10-19 23:27:17 -0700

    deploy1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-server "174.136.37.77", user: "deploy", roles: %w{app db web}
+ask(:password, nil, echo: false)
+server "174.136.37.77", user: "deploy", password: fetch(:password), roles: %w{app db web}
 
 # role-based syntax
 # ==================
